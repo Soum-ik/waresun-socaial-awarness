a40b182c667ad0d38bfaa16191a0c520b2f51f43
Stack Soumik. <110479389+Soum-ik@users.noreply.github.com>
2024-07-31 12:55:29 +0600

    Delete server directory

--- server/db/connection.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-db = mongoose.connect('mongodb+srv://soum-ik:frontenddev@cluster0.dunrodk.mongodb.net/warsum?retryWrites=true&w=majority&appName=Cluster0').
-  catch(error => handleError(error));
-try {
-  db = mongoose.connect('mongodb+srv://soum-ik:frontenddev@cluster0.dunrodk.mongodb.net/warsum?retryWrites=true&w=majority&appName=Cluster0');
-} catch (error) {
-  handleError(error);
-}
